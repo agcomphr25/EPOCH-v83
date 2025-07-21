bb65d03b8171e0d8a51b1ef6f196fcc3aa4a6e42
glenn51 <43698548-glenn51@users.noreply.replit.com>
2025-07-21 19:20:35 +0000

    Restored to '8b2c3d71917f50f070d4d20ef6ded899aa564208'
    Replit-Restored-To: 8b2c3d71917f50f070d4d20ef6ded899aa564208

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,6 @@ import Dashboard from "./pages/Dashboard";
 import OrderManagement from "./pages/OrderManagement";
 import DiscountManagement from "./pages/DiscountManagement";
 import OrderEntry from "./pages/OrderEntry";
-import OrderEntrySimple from "./pages/OrderEntrySimple";
 import OrderEntryTest from "./components/OrderEntryTest";
 import OrdersList from "./pages/OrdersList";
 import OrdersListSimple from "./pages/OrdersListSimple";
@@ -61,7 +60,6 @@ function App() {
                   <Route path="/" component={Dashboard} />
                   <Route path="/order-management" component={OrderManagement} />
                   <Route path="/order-entry" component={OrderEntry} />
-                  <Route path="/order-entry-simple" component={OrderEntrySimple} />
                   <Route path="/test-order-entry" component={OrderEntryTest} />
                   <Route path="/orders" component={OrdersList} />
                   <Route path="/orders-list" component={OrdersList} />
